d01deaaae4f86f02cdb3177824605cce89b1a8c4
JustDoIt <595265578@qq.com>
2020-03-05 18:20:14 +0800

    no message

--- es_example/go.mod
+++ es_example/go.mod
@@ -2,4 +2,7 @@ module knowledge/es_example
 
 go 1.13
 
-require github.com/elastic/go-elasticsearch v0.0.0 // indirect
+require (
+	github.com/fortytw2/leaktest v1.3.0 // indirect
+	gopkg.in/olivere/elastic.v2 v2.0.61
+)
--- es_example/main.go
+++ es_example/main.go
@@ -1,10 +1,8 @@
 package main
 
-
 import (
 	"fmt"
-
-	elastic "github.com/elastic/go-elasticsearch"
+	elastic "gopkg.in/olivere/elastic.v2"
 )
 
 type Tweet struct {
@@ -13,6 +11,28 @@ type Tweet struct {
 }
 
 func main()  {
-	client,err :=elastic.
+	client,err :=elastic.NewClient(elastic.SetSniff(false),elastic.SetURL("http://127.0.0.1:9200"))
+	if err != nil {
+		fmt.Println("connect es error", err)
+		return
+	}
+
+	fmt.Println("conn es succ")
+	tweet := Tweet{User: "olivere", Message: "Take Five"}
+	for i:=2;i<=100;i++ {
+		_, err = client.Index().
+			Index("twitter").
+			Type("tweet").
+			Id(fmt.Sprintf("%d",i)).
+			BodyJson(tweet).
+			Do()
+
+		if err != nil {
+			// Handle error
+			panic(err)
+			return
+		}
+	}
+	fmt.Println("insert succ")
 }
 
